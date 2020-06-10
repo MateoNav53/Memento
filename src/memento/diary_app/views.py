c2a71c16a277cc396c8e85139dfe08f48c98da8f
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-06-09 18:28:11 -0700

    home page, diary list, and new diary views added

--- src/memento/diary_app/views.py
+++ src/memento/diary_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Diary
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Here is the homepage")
+    return render(request, 'diary_app/template.html')
+
+def diary(request):
+    diaries = Diary.objects.all()
+    context = {
+        'diaries': diaries
+    }
+    return render(request, 'diary_app/diary.html', context)
+
+def new_diary(request):
+    if request.method =="POST":
+        create_new_diary = Diary.objects.create(
+            title=request.POST['title'],
+            # image=request.POST['image'],
+            content=request.POST['content']
+        )
+        create_new_diary.save()
+    return render(request, 'diary_app/new_diary.html')
