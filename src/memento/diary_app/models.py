3680ecb375b1e5f378a2bad2f6530e5c306e4a05
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-06-12 17:28:46 -0700

    added image field attributes

--- src/memento/diary_app/models.py
+++ src/memento/diary_app/models.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 # Create your models here.
 class Diary(models.Model):
     title = models.CharField(max_length=150)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='image/', blank=True, null=True)
     publish_date = models.DateTimeField(default=timezone.now)
     content = models.TextField()
