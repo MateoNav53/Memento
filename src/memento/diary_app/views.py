64e68dc851f3424958dadc948352ae62e361c8c4
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-06-12 17:39:50 -0700

    added redirect, image, and detail function

--- src/memento/diary_app/views.py
+++ src/memento/diary_app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Diary
 
 # Create your views here.
 def index(request):
-    return render(request, 'diary_app/template.html')
+    return render(request, 'diary_app/home.html')
 
 def diary(request):
     diaries = Diary.objects.all()
@@ -16,8 +16,22 @@ def new_diary(request):
     if request.method =="POST":
         create_new_diary = Diary.objects.create(
             title=request.POST['title'],
-            # image=request.POST['image'],
+            image=request.POST['image'],
             content=request.POST['content']
         )
         create_new_diary.save()
+        return redirect('/diary')
     return render(request, 'diary_app/new_diary.html')
+    
+    
+
+def detail(request, pk):
+    diary_detail = Diary.objects.get(pk=pk)
+    template_name = 'diary_app/diary_detail_page.html'
+    data = {
+        'title': diary_detail.title, 
+        'date': diary_detail.publish_date,
+        'content': diary_detail.content
+    }
+    return render(request, template_name, data)
+
